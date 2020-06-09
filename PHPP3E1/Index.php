0939cfa681df825e714d5efc8bd392ad4ecb2a4c
Jérôme DE PORRES <62543727+jeromedeporres@users.noreply.github.com>
2020-06-09 16:51:11 +0200

    Update Index.php

--- PHPP3E1/Index.php
+++ PHPP3E1/Index.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <title>PHPP3EXO01</title>
 </head>
     <body>
         <?php
             $number = 0;
-            while ($number <10)
+            while ($number < 10)
             {
             ?>
-            <p><?php echo $number; ?></p>
-            <?php
-                $number ++;
+            <p><?= $number ?></p><?php
+                $number++;
+            }
+            for ($number = 0; $number <10; $number++){
+                ?><p><?= $number ?></p><?php
             }
             ?>
     </body>
